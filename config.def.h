1a7976aada23234c93d92226438437dc0223047c
SubhadityMukherjee <msubhaditya@gmail.com>
2020-11-21 18:34:16 +0400

    build update

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 static const char *const autostart[] = {
-	"st", NULL,
+	"st -e /usr/bin/fish", NULL,
 	"sxhkd", NULL,
 	"pactl", NULL,
 	"dwmblocks", NULL,
@@ -76,7 +76,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4,  "-l", "20", "-g", "3", NULL };
 
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st -e /usr/bin/fish", NULL };
 
 static const char *qucmd[]  = { "qutebrowser", NULL };
 static const char *thunarcmd[]  = { "thunar", NULL };
